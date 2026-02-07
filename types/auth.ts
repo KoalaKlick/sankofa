d7da0e1078b4b8a4f59ec3bdacbf78839102d95d
EveryJE <kgyan19lf@outlook.com>
2026-02-07 20:00:31 +0000

    feat: add Supabase authentication types and define AuthState interface.

--- types/auth.ts
+++ types/auth.ts
@@ -1,4 +1,4 @@
-import type{ User as SupabaseUser, Session as SupabaseSession, AuthError as SupabaseAuthError  } from '@supabase/supabase-js'
+import { type User as SupabaseUser, type Session as SupabaseSession, type AuthError as SupabaseAuthError } from '@supabase/supabase-js'
 
 export type User = SupabaseUser
 export type Session = SupabaseSession

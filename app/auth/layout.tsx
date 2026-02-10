25761cc2b5e4fb925926f9f0a1fb803cff903c11
EveryJE <kgyan19lf@outlook.com>
2026-02-10 13:05:59 +0000

    feat(auth): wrap AfroTixLogo in Link component for improved navigation

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react"
 import AfricaMap from "../../components/auth/AfricaMap"
 
 import { AfroTixLogo } from "@/components/shared/AfroTixLogo"
+import Link from "next/link"
 
 export default function AuthLayout({
   children,
@@ -24,7 +25,9 @@ export default function AuthLayout({
         <div className="absolute top-1/2 left-6 right-6 lg:space-y-6 max-w-[12rem] sm:max-w-[15rem] md:max-w-xs pointer-events-none">
           <div className="flex items-center gap-2">
             {/* <Image height={40} width={40} src="/logo.png" alt="Sankofa Logo" className="bg-black h-20 w-auto" /> */}
-            <AfroTixLogo className="w-32 sm:w-38 md:w-40 lg:w-48 h-auto border border-black" />
+            <Link href="/" className="pointer-events-auto">
+              <AfroTixLogo className="w-32 sm:w-38 md:w-40 lg:w-48 h-auto border border-black" />
+            </Link>
           </div>
           <span className="mt-2 text-sm sm:text-base md:text-lg inline-block bg-secondary-50 font-medium text-foreground/80 ">
             Discover and book the hottest events across the continent.
